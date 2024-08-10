54224ec8983680ffe7bbd9aa961ae81771ce3de7
hi1re <scheremet316@gmail.com>
2024-08-10 23:31:31 +0300

    solved

---
+++ 1 - Easy/2-data-science-skills.sql
@@ -0,0 +1,7 @@
+--find candidates who are proficient in Python, Tableau, and PostgreSQL
+
+SELECT candidate_id
+FROM candidates
+WHERE skill IN ('Python', 'Tableau',   'PostgreSQL')
+GROUP BY candidate_id
+HAVING COUNT(skill) = 3;
